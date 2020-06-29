a3da8abf26daa1350404bac19304b7703c7fba82
mjl776 <mjl776@nyu.edu>
2020-06-28 22:47:49 -0400

    edited app.js file to create initial server, added client folder

---
+++ client/src/components/FoodGram.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FoodGram'
+}
+</script>
+
+<style scoped>
+</style>
--- server/app.js
+++ server/app.js
@@ -2,5 +2,5 @@ const express = require("express")();
 const http = require("http").Server(express);
 
 http.listen(3000, ()=> {
-    console.log("Listening at :3000...")
+    console.log("Listening at :3000...");
 });
