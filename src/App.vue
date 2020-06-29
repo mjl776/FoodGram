564ff3789e39d74e27f30063b966bf551c5d15c8
mjl776 <mjl776@nyu.edu>
2020-06-29 13:50:10 -0400

    created basic title to page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Title from './components/Title'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Title
+  }, 
+  data() {
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Title.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1> FoodGram </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+}
+
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+    font-size: 60px;
+    font-family: Georgia;
+    color: blue;
+
+  }
+</style>
