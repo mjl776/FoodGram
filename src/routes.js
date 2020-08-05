7bf8f406e43009d8ca50f101a9ad6973b55a7a14
Michael Lee <mlee@Ms-MBP-2.fios-router.home>
2020-08-04 22:06:43 -0400

    ccreated intial routing for single post with json placeholder and created single post component

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import FoodPages from './components/FoodPages.vue'
 import addComments from './components/addComments.vue'
+import singlePost from './components/singlePost.vue'
 export default [
     { path: '/', component: FoodPages },
-    { path: '/add', component: addComments }
+    { path: '/add', component: addComments },
+    { path: '/post/:id', component: singlePost }
 ]
