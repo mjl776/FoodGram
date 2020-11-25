694b4d19680d3465bc1e353e4a528a88f033514f
Michael Lee <mlee@10-20-243-228.dynapool.wireless.nyu.edu>
2020-11-24 19:42:14 -0500

    deleted posts folder and single post, to make all  posts accessible on one component

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,17 @@
 import FoodPages from './components/FoodPages.vue'
-import addComments from './components/Restaurants/Posts/comments/addComments.vue'
 import singleRestaurant from './components/Restaurants/singleRestaurant.vue'
 import showRestaurants from './components/Restaurants/showRestaurants.vue'
 import addRestaurantaccount from './components/Restaurants/addRestaurantaccount.vue'
 import addRestaurantPosts from './components/Restaurants/addRestaurantPosts.vue'
-import singlePost from './components/Restaurants/Posts/singlePost.vue'
 import signIn from './components/users/signIn.vue'
 import signUp from './components/users/signUp.vue'
 
 export default [
     { path: '/', component: FoodPages },
-    { path: '/addcomments', component: addComments },
     { path: '/restaurants/:id', component: singleRestaurant },
     { path: '/Restaurants', component: showRestaurants },
     { path: '/addRestaurantaccount', component: addRestaurantaccount},
     { path: '/Restaurants/:id/addRestaurantposts', component: addRestaurantPosts},
-    { path: '/Restaurants/Posts/:id', component: singlePost},
     { path: '/Accounts', component: signIn},
     { path: '/Accounts/sign-up', component: signUp}
 
